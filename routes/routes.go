a6cfa453c9a70c8caec9153ee698aad812e80072
CharlieDeepk <deeku753@gmail.com>
2024-11-06 12:16:40 +0530

    second commit

--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,15 @@
 package routes
+
+import (
+	"github.com/CharlieDeepk/go_ecommerce/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func userRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("/users/signup", controllers.SignUp())
+	incomingRoutes.POST("/users/login", controllers.LogIn())
+	incomingRoutes.POST("/admin/addproduct", controllers.ProductViewerAdmin())
+	incomingRoutes.GET("users/productview", controllers.SearchProduct())
+	incomingRoutes.GET("users/search", controllers.SearchProductByQuery())
+}
