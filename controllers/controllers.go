f1534bd6ea908ef8da838188a297ef6f8e04fbde
CharlieDeepk <deeku753@gmail.com>
2024-11-06 14:16:24 +0530

    controllers dir init

---
+++ controllers/controllers.go
@@ -0,0 +1,29 @@
+package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func HashPassword(rawPassword string) string {
+
+}
+
+func VerifyPassword(rawPassword string) bool {
+
+}
+
+func SignUp(email string, password string) gin.HandlerFunc {
+
+}
+
+func LogIn(email, string, password string) gin.HandlerFunc {
+
+}
+
+func ProductViewerAdmin() gin.HandlerFunc {
+
+}
+
+func SearchProduct() gin.HandlerFunc {}
+
+func SearchProductByQuery() gin.HandlerFunc {}
